ae368e82db02846de0a3320c4c41c0d30f54a088
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2015-02-13 00:39:42 +0900

    scp feature added

--- ossh.rb
+++ ossh.rb
@@ -54,42 +54,67 @@ class OnionSsh
   #TODO: to be configurable
   TEMP_DIR="/tmp/onion_ssh"
 
-  # _path_of_servers_ :: (Array) array of Server that Path generated
-  def initialize(path_of_servers)
-    @path_of_servers = path_of_servers
+  #TODO: use naitive ruby lib
+  def ssh(path_of_servers, command)
+    `#{ssh_str(path_of_servers, command)}`
   end
 
   #TODO: use naitive ruby lib
-  def ssh(command)
-    sshpass_command = _ssh(@path_of_servers.dup, "", command)
+  def ssh_without_dquote_command(path_of_servers, command)
+    `#{ssh_str_without_dquote_command(path_of_servers, command)}`
+  end
+
+  def ssh_str(path_of_servers, command)
+    return if path_of_servers.size == 0
+    if path_of_servers.size == 1
+      #wrap command with "
+      ssh_str_without_dquote_command(path_of_servers, "\"#{command}\"")
+    else
+      #wrap command with \"
+      ssh_str_without_dquote_command(path_of_servers, "\\\"#{command}\\\"")
+    end 
+  end
+
+  def ssh_str_without_dquote_command(path_of_servers, command)
+    sshpass_command = _ssh(path_of_servers.dup, "", command)
     #replace first \"
     sshpass_command.sub!(/\"/,"")
     #added remains \"
-    sshpass_command += "\"" * (@path_of_servers.size - 1)
+    sshpass_command += "\"" * (path_of_servers.size - 1)
   end
 
+
   #TODO: use naitive ruby lib
-  def scp(src_file, dst_file, option = "")
-    _path_of_servers = @path_of_servers.dup
+  def scp(src_file, path_of_servers, dst_file, option = "")
+    _path_of_servers = path_of_servers.dup
 
     #generate session uuid and make temp dir string
     temp_dir = "/tmp/onion_ssh/#{SecureRandom.uuid}/"
-    temp_file = "#{temp_dir}#{dst_file}"
+    temp_file = "#{temp_dir}#{File.basename(src_file)}"
 
     #first, local src file to edge server's temp dir
     #(/tmp/onion_ssh/<session uuid>/<files>)
     first = _path_of_servers.shift
-    `#{ssh_str(first, "mkdir -p #{temp_dir} >& /dev/null")}`
-    `#{scp_str(src_file, first, temp_file)}`
+    ssh([first], "mkdir -p #{temp_dir} >& /dev/null")
+    `#{one_scp_str(src_file, first, temp_file)}`
     
     #second to last - 1 , scp temp dir to temp dir
     last = _path_of_servers.pop
-    last_minus_one = _path_of_servers.pop
-    _scp_second_to_last_minus_one(_path_of_servers)
-    
-    #last - 1 to last , scp temp dir to dest dir
-    `#{ssh_str(last_minus_one, "mkdir -p #{temp_dir} >& /dev/null")}`
-    `#{scp_str(src_file, first_sv, temp_file)}`
+    temp_path = [first]
+    _second_to_last_minus_one = _path_of_servers
+    _second_to_last_minus_one.each do |sv|
+      temp_path << sv
+      ssh(temp_path,"mkdir -p #{temp_dir} >& /dev/null")
+    end
+
+    temp_path = [first]
+    _second_to_last_minus_one.each do |sv|
+      ssh(temp_path, "#{one_scp_str(temp_file, sv, temp_dir)}")
+      temp_path << sv
+    end
+
+    #last minus one's path=(temp_path above) and last
+    ssh(temp_path, "#{one_scp_str(temp_file, last, dst_file)}")
   end
 
 #################################
@@ -99,6 +124,7 @@ protected
   # _path_of_servers_ :: (Array) array of Server that Path generated
   # _sshpass_command_ :: (string) sshpass_command that generated
   # _user_exec_command :: (string)
+  # return :: (string) nested scp command by sshpass
   def _ssh(path_of_servers, sshpass_command, user_exec_command)
     if path_of_servers.size == 0
       return "#{sshpass_command} #{user_exec_command}"
@@ -106,23 +132,23 @@ protected
 
     server = path_of_servers.shift
     #shell command needs double quote's escape
-    sshpass_command += "\"#{ssh_str(server)}"
+    sshpass_command += "\"#{one_ssh_str(server)}"
     _ssh(path_of_servers, sshpass_command, user_exec_command)
   end
 
-  def ssh_str(server, command = "")
+  def one_ssh_str(server, command = "")
     " sshpass -p #{server.passwd}"\
     " ssh -l #{server.user}" \
     " -o StrictHostKeyChecking=no" \
     " #{server.ip} #{command}"
   end
 
-  def scp_str(src_file, server, dst_file)
+  def one_scp_str(src_file, server, dst_file)
     " sshpass -p #{server.passwd}"\
     " scp" \
     " -o StrictHostKeyChecking=no" \
-    " #{src_file}"
-    " #{server.user}@#{server.ip}"
+    " #{src_file}"\
+    " #{server.user}@#{server.ip}:#{dst_file}"
   end
 end
 
@@ -140,6 +166,10 @@ end
 pt = sc.path.get(s1, s3)
 #Path.dump(pt)
 
-ossh = OnionSsh.new(pt)
-puts ssh = ossh.ssh("hostname")
-puts `#{ssh}`
+#puts "==== basic ssh command hostname ==== "
+ossh = OnionSsh.new
+#puts ossh.ssh_str(pt, "hostname")
+puts ossh.ssh(pt, "hostname")
+
+#puts "==== basic scp === "
+ossh.scp("/tmp/go.sh", pt, "/tmp/")
