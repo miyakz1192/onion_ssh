fa02fc2c316091cba41f68f74cde1a65bc8963e6
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2015-02-13 01:49:22 +0900

    first added config.rb

---
+++ config.rb
@@ -0,0 +1,16 @@
+require "server_connections"
+
+@@sc = ServerConnections.new
+
+def load_from(file_name)
+end
+
+def account_group(group_name)
+end
+
+
+def server_group(group_name)
+end
+
+def server_connections(&block)
+end 
