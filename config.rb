52e82a9011fc952018c99dc003df1d1ffbaf7970
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2015-02-13 22:58:15 +0900

    config file parser modified

--- config.rb
+++ config.rb
@@ -2,6 +2,16 @@ require "./server_connections"
 
 $sc = ServerConnections.new
 $servers = []
+$server_groups = {}
+
+def find_server_by_name(name)
+  $servers.detect{|sv| sv.host == name}
+end
+
+def find_server_group_by_name(name)
+  k = $server_groups.keys.detect{|group_name| group_name == name}
+  $server_groups[k]
+end
 
 def load_from(file_name)
   open(file_name) do |f|
@@ -15,26 +25,73 @@ def load_from(file_name)
 end
 
 def account_group(login_user_name, passwd, &block)
-  return unless block.is_a?(Array)
+  return unless block.call.is_a?(Array)
 
-  block.each do |sv_name|
-    sv = $servers.detect{|sv| sv.host == sv_name}
-    next unless sv
+  block.call.each do |sv_name|
+    sv = find_server_by_name(sv_name)
+    raise "ERROR: no such server name=#{sv_name}" unless sv
     sv.user = login_user_name
     sv.passwd = passwd
   end
 end
 
 
-def server_group(group_name)
+def server_group(group_name, &block)
+  return unless block.call.is_a?(Array)
+
+  block.call.each do |sv_name|
+    sv = find_server_by_name(sv_name)
+    raise "ERROR: no such server name=#{sv_name}" unless sv
+    if $server_groups[group_name] == nil
+      $server_groups[group_name] = []
+    end
+    $server_groups[group_name] << sv
+  end
 end
 
 def server_connections(&block)
+  return unless block.call.is_a?(Array)
+
+  block.call.each do |sv_pair|
+    svg0 = find_server_group_by_name(sv_pair[0])
+    svg1 = find_server_group_by_name(sv_pair[1])
+    sv0 = find_server_by_name(sv_pair[0])
+    sv1 = find_server_by_name(sv_pair[1])
+
+    if sv0 && sv1
+      servers_left = [sv0]
+      servers_right = [sv1]
+    elsif svg0 && svg1
+      servers_left = svg0
+      servers_right = svg1
+    else
+      servers_left = svg0 || svg1
+      servers_right = [sv0 || sv1]
+    end
+
+    servers_left.product(servers_right).each do |sv_pair|
+      $sc.connect(from: sv_pair[0], to: sv_pair[1])
+    end
+  end
 end
 
+=begin
+begin
+  eval(open("test_ossh_config_sample.txt").read)
+rescue => e
+  puts e.message
+  puts e.backtrace()
+end
 
-#eval(open("config_sample.txt").read)
-
-load_from("/etc/hosts")
+puts "================servers===================="
 puts $servers.map{|sv| "#{sv.host},#{sv.ip},#{sv.user},#{sv.passwd}"}
+
+puts "================server_group===================="
+$server_groups.each_key do |key|
+  puts "group=>#{key}"
+  $server_groups[key].each do |sv|
+    puts "  mem=>#{sv.host}"
+  end
+end
+=end
 
